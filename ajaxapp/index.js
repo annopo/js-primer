c70bd12e795792d83192911adeecaffc336e3121
annopo <pecorium@gmail.com>
2020-11-24 19:19:16 +0900

    Learned HTTP requests using the Fetch API

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -10,3 +10,24 @@ const button = document.createElement("button");
 button.textContent = "Push Me";
 // body要素の子要素としてbuttonを挿入する
 document.body.appendChild(button);
+
+const userId = "js-primer-example";
+
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status); // => 200
+
+            //エラーレスポンスが返されたことを検知する
+            if (!response.ok) {
+                console.error("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    // JSONパースされたオブジェクトが渡される
+                    console.log(userInfo); // => {...}
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
