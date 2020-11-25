cadb1577c228525b74bb926127760ee075f4b027
annopo <pecorium@gmail.com>
2020-11-25 14:43:12 +0900

    Read the file using the readFile function

--- nodecli/main.js
+++ nodecli/main.js
@@ -4,11 +4,24 @@
 // const myModule = require("./my-module");
 // console.log(myModule.foo); // => "foo"
 
+
 // commanderモジュールをprogramオブジェクトとしてインポートする
 const program = require("commander");
-// コマンドライン引数をパースする
-program.parse(process.argv);
 
-// ファイルパスをprogram.args配列から取り出す
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
+// コマンドライン引数からファイルパスを取得する
+program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        //終了ステータス 1(一般的なエラー)としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
+    console.log(file);
+});
