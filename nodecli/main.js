6a601b5ef7c1dec6b373800694ce21109c0bbe9f
annopo <pecorium@gmail.com>
2020-11-25 14:30:15 +0900

    Installed packages and loaded modules

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+// console.log(process.argv);
+
+// my-module.jsモジュールをmyModuleオブジェクトとしてインポートする
+// const myModule = require("./my-module");
+// console.log(myModule.foo); // => "foo"
+
+// commanderモジュールをprogramオブジェクトとしてインポートする
+const program = require("commander");
+// コマンドライン引数をパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
